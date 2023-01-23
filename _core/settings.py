6ebcb6b95bda7c427ab3294a0dbcb5bda31b3b2f
João Maia <joaocarlosbgm@gmail.com>
2023-01-23 17:16:01 -0300

    initial commit

--- _core/settings.py
+++ _core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -45,7 +45,8 @@ DJANGO_APPS = [
 ]
 
 THIRD_PART_APPS = [
-    "rest_framework",
+    'rest_framework',
+    'oauth2_provider',
 ]
 
 MY_APPS = [
@@ -145,4 +146,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = "accounts.Account"
+AUTH_USER_MODEL = 'accounts.Account'
