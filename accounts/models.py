f34b93706a75308880f873f98cca26faff8ccd90
João Maia <joaocarlosbgm@gmail.com>
2023-01-23 16:51:07 -0300

    initial commit

---
+++ accounts/models.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Account(AbstractUser):
+     """
+     Classe modelo de usuários do um app
+     """
+     ...
