b5a3a6236bab43a7f8e8a89add4865a77e86d05c
João Maia <joaocarlosbgm@gmail.com>
2023-01-25 16:31:09 -0300

    feat: cnab view

--- cnab_api/views.py
+++ cnab_api/views.py
@@ -1,11 +1,27 @@
-from django.views.generic import FormView
-from .forms import UploadFileForm
-from django.http import HttpRequest, HttpResponse
 import datetime
+
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import FormView
+
+from .forms import UploadFileForm
 
 
 class CnabFormView(FormView):
     form_class = UploadFileForm
+
+    template_name = 'index.html'
+
+    def file_normalizer(self, value):
+        return {
+            'tipo': value[0:1],
+            'data': datetime.date(int(value[0:1]), int(value[5:7]), int(value[7:9])),
+            'valor': self.transactions_signal(int(value[0:1]), float(value[9:19]) / 100),
+            'cpf': value[19:30],
+            'cartao': value[30:42],
+            'hora_transacao': datetime.time(int(value[42:48]), int(value[44:46]), int(value[46:48])),
+            'dono_loja': value[48:62],
+            'nome_loja': value[62:81],
+        }
         
 
     def transactions_signal(self, type_transactions, value):
@@ -40,8 +56,10 @@ class CnabFormView(FormView):
         form = self.get_form(form_class=UploadFileForm)
         #files = request.FILES.get('./test_dataframe/CNAB.txt')
 
-        with open('./test_dataframe/CNAB.txt', 'r') as arquivo:
-            for value in arquivo:
+        with open('./test_dataframe/CNAB.txt', 'r') as file:
+            self.file_normalizer(file)
+
+            """ for value in file:
                 'tipo': value[0:1]
                 'data': datetime.date(int(value[0:1]), int(value[5:7]), int(value[7:9]))
                 'valor': self.transactions_signal(int(value[0:1]), float(value[9:19]) / 100)
@@ -49,7 +67,7 @@ class CnabFormView(FormView):
                 'cartao': value[30:42]
                 'hora_transacao': datetime.time(int(value[42:48]), int(value[44:46]), int(value[46:48]))
                 'dono_loja': value[48:62]
-                'nome_loja': value[62:81]
+                'nome_loja': value[62:81] """
 
         if form.is_valid():
             return self.form_valid(form)
