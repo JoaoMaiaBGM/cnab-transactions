e1ab63b018bdfa2a7e8973207f8e8ab94cc0572d
João Maia <joaocarlosbgm@gmail.com>
2023-01-29 21:14:09 -0300

    refactor: utils

--- cnab_api/utils.py
+++ cnab_api/utils.py
@@ -1,20 +1,35 @@
+from cnab_api.models import CnabApiModel
 from cnab_api.normalizers import FileNormalizer
-from cnab_api.repositories import ApiRepository
 
 
 class FileUtils():
 
-    def __init__(self, repository = None):
+    def __init__(self):
         self.normalizer = FileNormalizer()
-        self.repository = repository or ApiRepository()
+        self.model = CnabApiModel
+
+
+    def create_transaction(self, normalizer):
+        model = self.model(**normalizer)
+        model.save()
+
+
+    def get_transactions(self):
+        query = self.model.objects.all().order_by('nome_loja')
+        normalized_query = []
+        for transaction in query:
+            transaction = self.normalizer.query_normalizer(transaction)
+            normalized_query = normalized_query + [transaction]
+        return normalized_query
+
 
     def read_file(self, file):
         file = file.decode('utf-8')
         for value in file.splitlines():
             normalizer = self.normalizer.file_normalizer(value)
-            self.repository.create_transaction(normalizer)
+            self.create_transaction(normalizer)
         return file
 
 
     def filter(self, store):
-        return self.repository.get_transactions(), None
+        return self.get_transactions(), None
